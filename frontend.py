7ddbc1fc6b3e436ce53157d7fdb0f7e812a1c035
khalil lakhoua <arabickhalil@gmail.com>
2023-10-11 20:37:47 +0100

    un simple essai pour tkinter

--- frontend.py
+++ frontend.py
@@ -1,7 +1,26 @@
 import tkinter as tk
+from tkinter import ttk
+# if faut installer ceci
+from ttkthemes import ThemedStyle
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
 
-window = tk.Tk()
-window.title("Wireless networks analyser Project")
-greeting = tk.Label(text="Hello, Tkinter")
-greeting.pack()
-window.mainloop()
+# from plot_it import speed_test
+from analysis import signal_analysis
+sa = signal_analysis()
+
+root = tk.Tk()
+root.title("mohammed saif eddine et khalil")
+
+
+# Apply a modern theme to the application
+style = ThemedStyle(root)
+style.set_theme("yaru")
+
+# Create a themed button
+for network in sa.list_wifi_names():
+    button = ttk.Button(root, text=network)
+    button.pack(side=tk.LEFT, padx=5)  # Align button to the left
+
+
+root.mainloop()
