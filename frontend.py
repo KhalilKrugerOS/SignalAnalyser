7110088111a417b1a5e1ebc6224f0f3fe416bbc5
Mohamed Saifeddine Kamoun <mohamedsaifeddine.kamoun@insat.ucar.tn>
2023-10-13 14:19:58 +0100

    14:20 version - buttons and figures

--- frontend.py
+++ frontend.py
@@ -5,22 +5,36 @@ from ttkthemes import ThemedStyle
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 
+
+from plot_it import speed_test
+
 # from plot_it import speed_test
 from analysis import signal_analysis
 sa = signal_analysis()
+button_list = []
+
 
 root = tk.Tk()
-root.title("mohammed saif eddine et khalil")
+root.geometry("400x400")
+root.title("Mohamed Saifeddine et Khalil")
 
 
 # Apply a modern theme to the application
 style = ThemedStyle(root)
 style.set_theme("yaru")
 
-# Create a themed button
-for network in sa.list_wifi_names():
-    button = ttk.Button(root, text=network)
-    button.pack(side=tk.LEFT, padx=5)  # Align button to the left
+def refresh():
+    for button in button_list:
+        del button
+    # Create a themed button
+    for network in sa.list_wifi_names():
+        button_list.append(ttk.Button(root, text=network+"  "+str("%.2f" % sa.one_signal_power_dbm(network)),command=speed_test(network).show_graph))
+        button_list[-1].pack(side=tk.BOTTOM, padx=5)  # Align button to the left
 
+    del button_list[0]
+refresh_button = ttk.Button(root, text="Refresh", command = refresh)
+refresh_button.pack(side=tk.TOP, padx=5)
+
+refresh()
 
 root.mainloop()
--- plot_it.py
+++ plot_it.py
@@ -7,24 +7,31 @@ sa = signal_analysis()
 
 
 class speed_test:
-    def __init__(self):
+    def __init__(self, signal_name):
+        self.signal_name = signal_name
         self.x_data = []
         self.y_data = []
         self.figure = plt.figure()
         self.line, = plt.plot_date(self.x_data, self.y_data, '-')
+        self.animation = FuncAnimation(self.figure, self.update, interval=200,cache_frame_data=False)
 
     def update(self, frame):
+        print(self.signal_name)
         self.x_data.append(datetime.now())
-        self.y_data.append(sa.one_signal_power_dbm('Infinix SMART 5'))
+        self.y_data.append(sa.one_signal_power_dbm(self.signal_name))
         self.line.set_data(self.x_data, self.y_data)
         self.figure.gca().relim()
         self.figure.gca().autoscale_view()
         return self.line,
 
     def show_graph(self):
-        animation = FuncAnimation(self.figure, self.update, interval=200)
-        plt.show()
+        animation = self.animation
+        
+        print(self.animation)
+        self.animation = FuncAnimation(self.figure, self.update, interval=200,cache_frame_data=False)
+        print(self.animation)
+        plt().show()
 
 
-ts = speed_test()
-ts.show_graph()
+#ts = speed_test()
+#ts.show_graph()
